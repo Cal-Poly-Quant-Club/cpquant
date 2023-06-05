8881e97475dfe5ffba48388f229584a395c40aeb
Charlie Ray <charlie.ray320@gmail.com>
2023-06-05 15:30:00 -0700

    renamed folders

---
+++ cpquant/data.py
@@ -0,0 +1,22 @@
+class AlpacaClient:
+    def __init__(self):
+        pass
+
+    def get_account(self):
+        return 1000
+
+    def get_position(self):
+        return 1000
+
+    def get_barset(self):
+        return 1000
+
+    def get_last_trade(self)
+
+
+
+
+
+
+def cpquant_test():
+    print("Hello world from cal poly quant!")
